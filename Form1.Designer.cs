17ca05f2ade372e492b47f79c5a617134ed2c280
廣域-邱宇柔 <chiu.yu-jou@aecl.com.tw>
2025-02-12 09:44:13 +0800

    initial serialport project vertion

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace MyTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_Disconnect = new System.Windows.Forms.Button();
+            this.label_State = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.command_box = new System.Windows.Forms.TextBox();
+            this.btn_command = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(13, 77);
+            this.btn_Connect.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(100, 29);
+            this.btn_Connect.TabIndex = 1;
+            this.btn_Connect.Text = "Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // btn_Disconnect
+            // 
+            this.btn_Disconnect.Location = new System.Drawing.Point(13, 114);
+            this.btn_Disconnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Disconnect.Name = "btn_Disconnect";
+            this.btn_Disconnect.Size = new System.Drawing.Size(100, 29);
+            this.btn_Disconnect.TabIndex = 2;
+            this.btn_Disconnect.Text = "Disconnect";
+            this.btn_Disconnect.UseVisualStyleBackColor = true;
+            this.btn_Disconnect.Click += new System.EventHandler(this.btn_Disconnect_Click);
+            // 
+            // label_State
+            // 
+            this.label_State.AutoSize = true;
+            this.label_State.Location = new System.Drawing.Point(201, 21);
+            this.label_State.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_State.Name = "label_State";
+            this.label_State.Size = new System.Drawing.Size(39, 15);
+            this.label_State.TabIndex = 3;
+            this.label_State.Text = "State:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "14400"});
+            this.comboBox2.Location = new System.Drawing.Point(13, 45);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 23);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "9600";
+            // 
+            // command_box
+            // 
+            this.command_box.Location = new System.Drawing.Point(13, 176);
+            this.command_box.Name = "command_box";
+            this.command_box.Size = new System.Drawing.Size(100, 25);
+            this.command_box.TabIndex = 5;
+            // 
+            // btn_command
+            // 
+            this.btn_command.Location = new System.Drawing.Point(13, 208);
+            this.btn_command.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_command.Name = "btn_command";
+            this.btn_command.Size = new System.Drawing.Size(100, 29);
+            this.btn_command.TabIndex = 6;
+            this.btn_command.Text = "Enter";
+            this.btn_command.UseVisualStyleBackColor = true;
+            this.btn_command.Click += new System.EventHandler(this.btn_command_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 244);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(453, 130);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 562);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_command);
+            this.Controls.Add(this.command_box);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label_State);
+            this.Controls.Add(this.btn_Disconnect);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Button btn_Disconnect;
+        private System.Windows.Forms.Label label_State;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox command_box;
+        private System.Windows.Forms.Button btn_command;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace MyTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+            btn_Disconnect.Enabled = false;
+            btn_Connect.Enabled = true;
+            label_State.Text = "Coose port to connect.";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort1.Close();
+            serialPort1.Dispose();
+        }
+
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            try{
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = int.Parse(comboBox2.Text);
+                
+                if (serialPort1.IsOpen){
+                    label_State.Text = serialPort1.PortName + "port isent openned.";
+                }
+                else{
+                    serialPort1.Open();
+                    if (serialPort1.IsOpen)
+                    {
+                        label_State.Text = serialPort1.PortName + "connect success.";
+                        btn_Connect.Enabled = false;
+                        btn_Disconnect.Enabled = true;
+                    }
+                    else
+                    {
+                        label_State.Text = serialPort1.PortName + "Connect unsuccess.";
+                    }
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Disconnect_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            if (serialPort1.IsOpen)
+            {
+                label_State.Text = serialPort1.PortName + "disconnect unsuccess.";
+            }
+            else
+            {
+                label_State.Text = "Coose port to connect.";
+                btn_Connect.Enabled = true;
+                btn_Disconnect.Enabled = false;
+            }
+        }
+
+
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!btn_Connect.Enabled)
+            {
+                Variable.RxString = serialPort1.ReadExisting();
+                Variable.RxString = Variable.RxString.Replace("\u0006", "[ACK]\n");
+                Invoke(new EventHandler(display_info));
+            }
+            else { }
+        }
+
+        private void display_info(object s, EventArgs e) {
+            richTextBox1.AppendText(Variable.RxString);
+        }
+
+        private void btn_command_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine(command_box.Text + '\r' + '\n');
+            }
+            else { }
+        }
+
+        
+    }
+}
